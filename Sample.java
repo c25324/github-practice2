b7335b41479a42b4812b97dca00604d024b67f93
suntech\c25324 <c25324@suntech.jp>
2026-06-15 15:23:47 +0900

    バグを修正しました#4

--- Sample.java
+++ Sample.java
@@ -1,7 +1,15 @@
+import java.io.*;
+
 public class Sample {
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
-        System.out.println(a/b);
+        
+        try{
+            System.out.println(a/b);
+        }catch (ArithmeticException e) {
+            System.out.println("エラー:0で割ることはできません");
+        }
+        
     }
 }
