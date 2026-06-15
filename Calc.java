62f048d42c99051a39c9150025969daa074c8e55
suntech\c25324 <c25324@suntech.jp>
2026-06-15 16:06:04 +0900

    追加機能を作成しました

--- Calc.java
+++ Calc.java
@@ -6,12 +6,17 @@ public class Calc {
         return a + b;
     }
 
+    public static int sub(int a, int b){
+        return a - b;
+    }
 
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
+
         int a = stdIn.nextInt();
         int b = stdIn.nextInt();
 
-        System.out.println("和: " + add(a,b));
+        System.out.println("和: " + add(a, b));
+        System.out.println("差: " + sub(a, b));
     }
-} 
+}
